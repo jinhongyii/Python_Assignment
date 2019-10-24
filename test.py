c5eed0abe9656d6bc3cac27c7cdfc9680b2694f9
jinhongyi <3231950289@qq.com>
2019-10-24 22:44:45 +0800

    fix test

--- test.py
+++ test.py
@@ -3,8 +3,12 @@ import os
 if os.path.exists("temp"):
     os.system("rm -rf ./temp")
 os.makedirs("temp")
-for i in range(50):
-    print("./python_interpreter "+str(i)+" > temp/test"+str(i)+".out")
-    os.system("./python_interpreter "+str(i)+" > temp/test"+str(i)+".out")
+for i in range(13):
+    inst ="./python_interpreter "+str(i)+" > temp/test"+str(i)+".out"
+    print(inst)
+    os.system(inst)
     if not filecmp.cmp("testData/test"+str(i)+".out","temp/test"+str(i)+".out"):
+        f = open("testData/test" + str(i) + ".in", mode='r')
+        title = f.readline()[1:]
+        print("test", i, "wrong:", title)
         print(i,"wrong")
